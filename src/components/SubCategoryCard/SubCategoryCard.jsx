97a61931238558bbda42b9c51618bda5b35927b2
hparvez307 <hparvez307@gmail.com>
2023-05-19 11:56:52 +0600

    sweet alert for view details

--- src/components/SubCategoryCard/SubCategoryCard.jsx
+++ src/components/SubCategoryCard/SubCategoryCard.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
-const SubCategoryCard = ({toy}) => {
+const SubCategoryCard = ({ toy }) => {
 
-  const {name,rating,sellerEmail, description, sellerName, subCategory, photo, price, quantity} = toy;
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
-    return (
-        <div className="card mx-auto w-96 bg-base-100  shadow-inner shadow-rose-300">
-  <figure><img className='p-2' src={photo}alt="Shoes" /></figure>
-  <div className="card-body text-center">
-  <h2 className="text-3xl text-bold ">  {name}</h2>
-  <p><span className='text-bold '>price:</span>  ${price}</p>
-    <p><span className='text-bold '>Rating:</span> {rating}</p>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary">Buy Now</button>
+  const { name, rating, _id, photo, price } = toy;
+
+
+  const handleViewDetails = () => {
+
+    // if the user not logged in show a sweet alert and redirect to login page 
+    if (!user) {
+      Swal.fire({
+        title: 'Login!',
+        text: 'You have to log in first to view details!',
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      })
+      navigate(`/toyDetails/${_id}`);
+    } else {
+      navigate(`/toyDetails/${_id}`);
+    }
+  }
+
+  return (
+    <div className="card mx-auto w-96 bg-base-100  shadow-inner shadow-rose-300">
+      <figure><img className='p-2' src={photo} alt="Shoes" /></figure>
+      <div className="card-body text-center">
+        <h2 className="text-3xl text-bold ">  {name}</h2>
+        <p><span className='text-bold '>price:</span>  ${price}</p>
+        <p><span className='text-bold '>Rating:</span> {rating}</p>
+        <div className="card-actions justify-center">
+          <button onClick={handleViewDetails} className='btn btn-error'>View Details</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default SubCategoryCard;
