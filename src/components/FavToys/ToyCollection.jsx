b3726a18aa043be20fb1a1a9b8aa69034ad3f200
hparvez307 <hparvez307@gmail.com>
2023-05-20 13:24:37 +0600

    toy collecton setup

--- src/components/FavToys/ToyCollection.jsx
+++ src/components/FavToys/ToyCollection.jsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ToyCollectionCard from './ToyCollectionCard';
 
 const ToyCollection = () => {
-
-
     const [toys, setToys] = useState([]);
-
 
     useEffect(() => {
         fetch('https://toy-land-server.vercel.app/toys')
@@ -23,16 +20,11 @@ const ToyCollection = () => {
             <h1 className='text-5xl text-bold text-center my-5'>Toy Collection</h1>
 
             <div className='grid grid-cols-1  justify-center gap-5 md:grid-cols-3'>
-
-
-
-
                 {
                     toys.map((toy) => <ToyCollectionCard toy={toy} key={toy._id}></ToyCollectionCard>)
                 }
             </div>
         </div>
-
     );
 };
 
