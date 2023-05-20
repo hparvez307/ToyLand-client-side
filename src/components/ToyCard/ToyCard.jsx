079f2f93fd6f30a3a4bc7d0342ae72fcc3f3c21b
hparvez307 <hparvez307@gmail.com>
2023-05-20 13:33:58 +0600

    toy card update

--- src/components/ToyCard/ToyCard.jsx
+++ src/components/ToyCard/ToyCard.jsx
@@ -13,10 +13,10 @@ const ToyCard = ({ photo }) => {
     }, []);
 
     return (
-        <div className="card shadow-2xl card-compact border p-7 toyimg w-96 bg-base-100 shadow-xl">
-            
+        <div className="card shadow-2xl card-compact border p-7 toyimg w-96 bg-base-100">
+
             <figure><img data-aos="flip-left" className='h-80 rounded w-full' src={photo} alt="Shoes" /></figure>
-            
+
         </div>
     );
 };
